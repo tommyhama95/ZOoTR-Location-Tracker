b39a929eb48d6badc4804231ee1cc9844d481e6f
TommyHama95 <tommyhamarsnes@gmail.com>
2021-01-04 17:19:27 +0100

    Displays card titles, refactored location.json again, started styling

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
-let locations;
+const cardContainer = document.querySelector(".location-container");
 
 async function getLocations() {
     fetch("./locations.json")
         .then(result => result.json())
         .then(data => {
-            locations = data.locations;
-            console.log(locations)
+            console.log(data.locations)
             displayLocationCards(data.locations)
         });
 }
 
-function displayLocationCards(arr) {
+function displayLocationCards(locations) {
+    locations.map(area => displayArea(area))
+}
 
+function displayArea(area) {
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+    
+    for(let i = 0; i < area.length; i++) {
+        const name = Object.getOwnPropertyNames(area[i])[0];
+        if(i === 0) { // AKA Title for the area
+            const cardTitle = document.createElement("div");
+            cardTitle.setAttribute("class", "card-title");
+            cardTitle.innerText = name;
+            card.appendChild(cardTitle);
+            continue;
+        }
+
+
+    }
+
+    cardContainer.appendChild(card);
 }
 
 
